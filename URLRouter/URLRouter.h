94f703edb42bcdab7f697f3e432c6c73428abc1f
Alberto Chamorro <alberto.chamorro@hudl.com>
2016-11-20 16:34:48 +0000

    Initial Commit

---
+++ URLRouter/URLRouter.h
@@ -0,0 +1,19 @@
+//
+//  URLRouter.h
+//  URLRouter
+//
+//  Created by Alberto Chamorro on 20/11/2016.
+//  Copyright © 2016 Alberto Chamorro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URLRouter.
+FOUNDATION_EXPORT double URLRouterVersionNumber;
+
+//! Project version string for URLRouter.
+FOUNDATION_EXPORT const unsigned char URLRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLRouter/PublicHeader.h>
+
+
